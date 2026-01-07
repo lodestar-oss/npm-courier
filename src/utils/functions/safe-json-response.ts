6afbf1b316c4ef04fd3223ba24f3c4d65e41db7e
Ryan Luong <lumintriet@gmail.com>
2026-01-07 22:30:51 +0700

    Improve safe json response function typing.

--- src/utils/functions/safe-json-response.ts
+++ src/utils/functions/safe-json-response.ts
@@ -1,18 +1,17 @@
-import { err, ok } from "neverthrow";
+import { err, ok, Result } from "neverthrow";
 
-export async function safeJsonResponse(response: Response) {
+export async function safeJsonResponse(
+  response: Response
+): Promise<Result<any, Error>> {
   try {
     const json = await response.json();
     return ok(json);
   } catch (error) {
-    if (error instanceof TypeError) {
-      return err(error);
-    }
-    if (error instanceof SyntaxError) {
+    if (error instanceof TypeError || error instanceof SyntaxError) {
       return err(error);
     }
     const unknownError = new Error(
-      "Unknown error occurred while parsing JSON from response",
+      "Unknown error occurred while reading response body as JSON",
       {
         cause: error,
       }

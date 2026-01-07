dcfdf726a8f428d91a99157771b9f4860fc5dc84
Ryan Luong <lumintriet@gmail.com>
2026-01-07 22:41:33 +0700

    Get package metadata function.

---
+++ src/get-package-metedata.ts
@@ -0,0 +1,29 @@
+import {
+  ABBREVIATED_METADATA_ACCEPT_HEADER,
+  NPM_REGISTRY_URL,
+} from "@/utils/constants";
+import { getJsonResponse } from "@/utils/functions/get-json-response";
+
+export async function getPackageMetadata({
+  name,
+  format = "abbreviated",
+}: {
+  name: string;
+  format?: "abbreviated" | "full";
+}) {
+  const acceptHeader =
+    format === "abbreviated"
+      ? ABBREVIATED_METADATA_ACCEPT_HEADER
+      : "application/json";
+
+  const getJsonResult = await getJsonResponse({
+    url: `${NPM_REGISTRY_URL}/${name}`,
+    options: { headers: { Accept: acceptHeader } },
+  });
+
+  if (getJsonResult.isErr()) {
+    return { success: false, error: getJsonResult.error };
+  }
+
+  return { success: true, data: getJsonResult.value };
+}
